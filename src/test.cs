0aaf89183d1bb024f0dfbbeacd538dd396613515
NopalAul <mnaufal0605@gmail.com>
2024-05-12 17:03:42 +0700

    feat: add folder template

---
+++ src/test.cs
@@ -0,0 +1,17 @@
+// simple hello world c# program
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// how to run c# program
+// csc test.cs
+// mono test.exe
