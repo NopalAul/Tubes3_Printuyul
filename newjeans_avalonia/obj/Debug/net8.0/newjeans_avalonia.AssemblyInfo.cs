49a83a540e1bd639be17e58229b0586296ea15c7
NopalAul <mnaufal0605@gmail.com>
2024-05-12 21:42:41 +0700

    fix: no font

--- newjeans_avalonia/obj/Debug/net8.0/newjeans_avalonia.AssemblyInfo.cs
+++ newjeans_avalonia/obj/Debug/net8.0/newjeans_avalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newjeans_avalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a85378bf589413d72455b0b1092af577c68e471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25e4270cf6a8884d5c2ac6a828f4d972a60eff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("newjeans_avalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newjeans_avalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
