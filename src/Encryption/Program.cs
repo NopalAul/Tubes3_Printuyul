31f4e41f782a9df853143194345dbeb80df8d5a9
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2024-06-09 12:54:59 +0700

    Fix : ecnryption

--- src/Encryption/Program.cs
+++ src/Encryption/Program.cs
@@ -1,5 +1,5 @@
 ﻿string biodataCsvFilePath = "encrypted_biodata.csv";
 string fingerprintCsvFilePath = "decrypted_sidik_jari.csv";
-string dbPath = "../FingerprintApi/HeheEncryptedData.db";
+string dbPath = "../FingerprintApi/EncryptedData.db";
 InsertBiodataToCSV.InsertBiodata(biodataCsvFilePath, dbPath);
 InsertSidikJariToCSV.InsertSidikJari(fingerprintCsvFilePath, dbPath);
